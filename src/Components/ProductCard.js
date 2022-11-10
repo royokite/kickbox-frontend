ce6d2849c5e511890a106fc3a446b011ac39438a
Titus Mumo <titusmumo@gmail.com>
2022-11-10 23:42:01 +0300

    Product card & css

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -13,11 +13,16 @@ const ProductCard = ({product}) => {
           {product.product_name}
         </div>
 
-        <div className='px-0 mt-2'>
+        {/* <div className='px-0 mt-2'>
         <span className='class="inline-block bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-700 mr-2 mb-2 pb-2 text-lg'>
           ${product.price}
         </span>
-      </div>
+      </div> */}
+
+        <div class="productPrice">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">{product.price}</span>
+            <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
       </div>
     </div>
     </>
