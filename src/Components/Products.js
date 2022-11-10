0cec2982ae565851ee96076134de600fa5debd8e
Titus Mumo <titusmumo@gmail.com>
2022-11-10 22:12:56 +0300

    fetch products

---
+++ src/Components/Products.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+
+const ProductCollection = ({products, action, removeCard}) => {
+
+  const displayProducts = products.map(product => {
+    return (
+      <>
+      <ProductCard key={product.id} product={product} action={action} removeCard={removeCard} />
+      </>
+    )
+  })
+
+  return (
+    <>
+    <div>
+    </div>
+    <div className=' grid grid-rows-4 grid-flow-col gap-7 h-fit ml-5 mr-5'>
+        {displayProducts}
+    </div>
+    </>
+
+  )
+}
+
+export default ProductCollection 
