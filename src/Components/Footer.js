ea27ffebd73194051290229eab3e0db148d98e89
RoyOkite <royokite@gmail.com>
2022-11-15 05:16:47 +0300

    Deploy error fix 1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer className="footer bg-violet-500">
       <div className="policies">
-        <a className="hover:opacity-75" href="#"> Privacy Policy </a>
-        <a className="hover:opacity-75" href="#"> Terms & Conditions </a>
-        <a className="hover:opacity-75" href="#"> Returns Policy </a>
+        <a className="hover:opacity-75" href="/products"> Privacy Policy </a>
+        <a className="hover:opacity-75" href="/products"> Terms & Conditions </a>
+        <a className="hover:opacity-75" href="/products"> Returns Policy </a>
       </div>   
       <div className="items-center grid-flow-col mt-2">
           <p>Copyright © 2022 - All rights reserved</p>
